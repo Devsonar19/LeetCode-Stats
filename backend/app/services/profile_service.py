bfe8e31bf4d58bdbbda3b37429e0286714ebb608
https://github.com/Devsonar19/My-Notes.git <devsonar19@outlook.com>
2026-03-06 23:57:32 +0530

    now we can fetch many other details

--- backend/app/services/profile_service.py
+++ backend/app/services/profile_service.py
@@ -1,12 +1,72 @@
 from app.services.graphql_client import execute_query
 
 PROFILE_QUERY = """
-query getProfile($username: String!) {
+query getUserDashboard($username: String!) {
+
   matchedUser(username: $username) {
     username
+
     profile {
       realName
+      userAvatar
       ranking
+      reputation
+    }
+
+    badges {
+      displayName
+      icon
+    }
+
+    activeBadge {
+      displayName
+      icon
+    }
+
+    submitStatsGlobal {
+      acSubmissionNum {
+        difficulty
+        count
+        submissions
+      }
+
+      totalSubmissionNum {
+        difficulty
+        count
+        submissions
+      }
+    }
+
+    submissionCalendar
+  }
+
+  recentSubmissionList(username: $username, limit: 20) {
+    title
+    titleSlug
+    timestamp
+    statusDisplay
+    lang
+  }
+
+  userContestRanking(username: $username) {
+    attendedContestsCount
+    rating
+    globalRanking
+    topPercentage
+  }
+
+  allQuestionsCount {
+    difficulty
+    count
+  }
+
+  activeDailyCodingChallengeQuestion {
+    date
+    link
+    question {
+      title
+      titleSlug
+      difficulty
     }
   }
 }
@@ -14,7 +74,19 @@ query getProfile($username: String!) {
 
 async def get_profile(username: str):
     data = await execute_query(PROFILE_QUERY, {"username": username})
-    return data["data"]["matchedUser"]
+
+    if "data" not in data:
+        return {"error": "User not found", "details": data}
+    
+    print(data)
+    
+
+    result = data["data"]
+    return{
+      "profile":  result["matchedUser"],
+      "allQuestionsCount": result["allQuestionsCount"],
+      "activeDailyCodingChallengeQuestion": result["activeDailyCodingChallengeQuestion"]
+    }
 
 BADGES_QUERY = """
 query getBadges($username: String!) {
