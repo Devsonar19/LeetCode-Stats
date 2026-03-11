3a8882ab0d505024b6ed9e9bc46decfa17865f94
https://github.com/Devsonar19/My-Notes.git <devsonar19@outlook.com>
2026-03-11 23:47:01 +0530

    added app drawer, shaping mobile interface

--- frontend/leetcode_stats/lib/features/dashboard/view/mobile/dashboard_mobile.dart
+++ frontend/leetcode_stats/lib/features/dashboard/view/mobile/dashboard_mobile.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 
 import '../../../../services/api_service.dart';
+import '../../../../shared/layout/app_drawer.dart';
 
 class DashboardMobile extends StatefulWidget {
   const DashboardMobile({super.key});
@@ -23,76 +24,80 @@ class _DashboardMobileState extends State<DashboardMobile> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("LeetCode Stats"),
-        centerTitle: true,
-      ),
-
-      body: FutureBuilder<Map<String, dynamic>>(
-          future: profileData,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            }
-
-            if (snapshot.hasError) {
-              return Center(child: Text("Error: ${snapshot.error.toString()}"));
-            }
-
-            if (!snapshot.hasData) {
-              return const Center(child: Text("No data"));
-            }
-
-            final data = snapshot.data!;
-            final user = data["profile"];
-            final profile = user["profile"];
-
-            return ListView(
-              padding: const EdgeInsets.all(20),
-              children: [
-                Text(
-                  "Hello, ${profile["realName"]}",
-                  style: const TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-
-                const SizedBox(height: 10),
-
-                Image.network(
-                    profile["userAvatar"],
-                    height: 90,
-                  errorBuilder: (context, error, stackTrace){
-                      return Icon(Icons.person, size: 50 );
-                  }
-                ),
-
-                const SizedBox(height: 20),
-
-                Text(
-                  "Rank: ${profile["ranking"]}",
-                  style: const TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-
-                const SizedBox(height: 20),
-
-                Text(
-                  "Daily Question \n${data["activeDailyCodingChallengeQuestion"]["question"]["title"]}",
-                  style: const TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-
-              ]
-            );
+    return FutureBuilder<Map<String, dynamic>>(
+        future: profileData,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
           }
-      ),
+
+          if (snapshot.hasError) {
+            return Center(child: Text("Error: ${snapshot.error.toString()}"));
+          }
+
+          if (!snapshot.hasData) {
+            return const Center(child: Text("No data"));
+          }
+
+          final data = snapshot.data!;
+          final user = data["profile"];
+          final profile = user["profile"];
+
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text("LeetCode Stats"),
+              centerTitle: true,
+            ),
+            drawer: AppDrawer(
+              userData: data["profile"],
+              isDarkMode: false,
+              onToggleTheme: (){},
+            ),
+            body: ListView(
+                padding: const EdgeInsets.all(20),
+                children: [
+                  Text(
+                    "Hello, ${profile["realName"]}",
+                    style: const TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                  const SizedBox(height: 10),
+
+                  Image.network(
+                      profile["userAvatar"],
+                      height: 90,
+                      errorBuilder: (context, error, stackTrace){
+                        return Icon(Icons.person, size: 50 );
+                      }
+                  ),
+
+                  const SizedBox(height: 20),
+
+                  Text(
+                    "Rank: ${profile["ranking"]}",
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                  const SizedBox(height: 20),
+
+                  Text(
+                    "Daily Question \n${data["activeDailyCodingChallengeQuestion"]["question"]["title"]}",
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                ]
+            ),
+          );
+        }
     );
   }
 }
---
+++ frontend/leetcode_stats/lib/shared/layout/app_drawer.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class AppDrawer extends StatelessWidget {
+  final Map<String, dynamic>? userData;
+  final bool isDarkMode;
+  final VoidCallback onToggleTheme;
+
+  const AppDrawer({
+    super.key,
+    required this.userData,
+    required this.isDarkMode,
+    required this.onToggleTheme,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final profile = userData?["profile"];
+
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.primaryContainer,
+            ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    radius: 30,
+                    backgroundImage: profile != null
+                        ? NetworkImage(profile["userAvatar"])
+                        : null,
+                    child: profile == null
+                        ? const Icon(Icons.person, size: 30,)
+                        : null,
+                  ),
+
+                  const SizedBox(height: 10),
+
+                  Text(
+                    userData?["username"] ?? "Guest",
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+          ),
+
+          ListTile(
+            leading: Icon(Icons.person),
+            title: const Text("Profile"),
+            onTap: (){
+              //opens secondary profile page
+            },
+          ),
+
+          SwitchListTile(
+              title: const Text("Dark Mode"),
+              secondary: Icon(Icons.dark_mode),
+              value: isDarkMode,
+              onChanged: (value){
+                onToggleTheme();
+              }
+          ),
+
+          const Divider(),
+
+          ListTile(
+            leading: Icon(Icons.computer),
+            title: const Text("About Developer"),
+            onTap: (){
+              //opens about developer page
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
