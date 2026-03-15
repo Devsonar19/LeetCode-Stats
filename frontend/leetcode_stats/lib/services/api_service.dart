9416a13ec58615dc621e0a8afb2c7368389f52ad
https://github.com/Devsonar19/My-Notes.git <devsonar19@outlook.com>
2026-03-15 16:14:53 +0530

    now using render backend url

--- frontend/leetcode_stats/lib/services/api_service.dart
+++ frontend/leetcode_stats/lib/services/api_service.dart
@@ -2,8 +2,8 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static const baseUrlForWeb = "http://127.0.0.1:8000";
-  static const baseUrlForAndroid = "http://127.0.0.1:8000";
+  static const baseUrlForWeb = "https://leetcode-stats-backend-txi3.onrender.com";
+  static const baseUrlForAndroid = "https://leetcode-stats-backend-txi3.onrender.com";
   // using 10.0.2.2 url for Android emulator
 
 
