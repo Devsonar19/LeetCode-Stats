d64f2fb21463b613815def83012dddf7f5c454f6
https://github.com/Devsonar19/My-Notes.git <devsonar19@outlook.com>
2026-04-22 20:39:33 +0530

    got the application working on Linux, will soon start optimizing it for big screen :)

--- frontend/leetcode_stats/linux/runner/my_application.cc
+++ frontend/leetcode_stats/linux/runner/my_application.cc
@@ -32,7 +32,25 @@ static void my_application_activate(GApplication* application) {
   // in case the window manager does more exotic layout, e.g. tiling.
   // If running on Wayland assume the header bar will work (may need changing
   // if future cases occur).
-  gboolean use_header_bar = TRUE;
+    gboolean use_header_bar = TRUE;
+
+    // 1. Get the absolute path to the running executable dynamically
+    g_autoptr(GError) error = nullptr;
+    g_autofree gchar* exe_path = g_file_read_link("/proc/self/exe", &error);
+
+    if (exe_path != nullptr) {
+        // 2. Extract the directory containing the executable
+        g_autofree gchar* base_dir = g_path_get_dirname(exe_path);
+
+        // 3. Build the path to the logo (base_dir/resources/app_logo.png)
+        g_autofree gchar* logo_path = g_build_filename(base_dir, "resources", "app_logo.png", nullptr);
+
+        // 4. Set the icon
+        gtk_window_set_icon_from_file(window, logo_path, nullptr);
+    } else {
+        // If we fail to find the path, log it so it doesn't fail silently
+        g_warning("Failed to resolve executable path for icon: %s", error->message);
+    }
 #ifdef GDK_WINDOWING_X11
   GdkScreen* screen = gtk_window_get_screen(window);
   if (GDK_IS_X11_SCREEN(screen)) {
@@ -45,11 +63,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "leetcode_stats");
+    gtk_header_bar_set_title(header_bar, "LeetCode Stats");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "leetcode_stats");
+    gtk_window_set_title(window, "LeetCode Stats");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
