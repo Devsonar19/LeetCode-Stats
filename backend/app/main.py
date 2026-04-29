144bf83998aa890a5d69c2ce8e8e1672ca4c628c
https://github.com/Devsonar19/My-Notes.git <devsonar19@outlook.com>
2026-04-29 12:45:52 +0530

    fixed CORS issue

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from app.api.router import api_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title = 'LeetCode Stats App',
     version = '1.0.0'
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(api_router)
